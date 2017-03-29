f93d27f2925693f68932ac148e3cc76ccc558a13
pradosavljevic <pradosavljevic@student.tgm.ac.at>
2017-03-29 20:47:01 +0200

    Commit2

--- src/petar/Test1703.java
+++ src/petar/Test1703.java
@@ -3,7 +3,9 @@ package petar;
 public class Test1703 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hallo Petar!");
+		System.out.println("Hallo Petar!");
+		System.out.println("Hallo Petar!");
 
 	}
 
